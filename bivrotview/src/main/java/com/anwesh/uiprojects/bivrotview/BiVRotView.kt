96b620a91694d9f17c4b534c96f4022f22c23801
Anwesh43 <anweshthecool0@gmail.com>
2020-01-14 10:50:48 +0530

    adding cosntants and libraries

--- bivrotview/src/main/java/com/anwesh/uiprojects/bivrotview/BiVRotView.kt
+++ bivrotview/src/main/java/com/anwesh/uiprojects/bivrotview/BiVRotView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.bivrotview
 /**
  * Created by anweshmishra on 14/01/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
