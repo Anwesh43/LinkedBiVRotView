5a3dffa8dca093c32939c090948f245295b4b420
Anwesh43 <anweshthecool0@gmail.com>
2020-01-14 10:57:30 +0530

    adding Canvas extension methods

--- bivrotview/src/main/java/com/anwesh/uiprojects/bivrotview/BiVRotView.kt
+++ bivrotview/src/main/java/com/anwesh/uiprojects/bivrotview/BiVRotView.kt
@@ -14,13 +14,39 @@ import android.app.Activity
 
 val nodes : Int = 5
 val lines : Int = 2
-val scGap : Float = 0.02f
+val scGap : Float = 0.02f / lines
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 30f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBiVRotLine(size : Float, scale : Float, paint : Paint) {
+    val sc : Float = scale.divideScale(0, 2)
+    val sf : Float = scale.divideScale(1, 2).sinify()
+    for (j in 0..(lines - 1)) {
+        save()
+        rotate(-deg * sf.divideScale(j, lines) * (1f - 2 * j))
+        drawLine(0f, 0f, 0f, -size * sc, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawBVRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawBiVRotLine(size, scale, paint)
+    restore()
+}
