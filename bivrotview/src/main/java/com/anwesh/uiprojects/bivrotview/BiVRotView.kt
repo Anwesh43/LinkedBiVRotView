b39e4b2d035a138d41f5677da62f4c9f9c5bea2a
Anwesh43 <anweshthecool0@gmail.com>
2020-01-14 11:02:06 +0530

    adding Animator class

--- bivrotview/src/main/java/com/anwesh/uiprojects/bivrotview/BiVRotView.kt
+++ bivrotview/src/main/java/com/anwesh/uiprojects/bivrotview/BiVRotView.kt
@@ -20,6 +20,7 @@ val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val deg : Float = 30f
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -84,6 +85,34 @@ class BiVRotView(ctx : Context) : View(ctx) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
